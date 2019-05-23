b9723effa291da351262b1746f4561e78126f940
avdata99 <andres@data99.com.ar>
2019-05-22 23:37:26 -0300

    mas cerca

--- ckanext/google_drive_import/plugin.py
+++ ckanext/google_drive_import/plugin.py
@@ -1,68 +1,30 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+import logging
+log = logging.getLogger(__name__)
+
 
 class Google_Drive_ImportPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IDatasetForm, inherit=True)
-
-    # IDatasetForm
-
-    def _add_country_codes(self, schema):
-        # Add our custom country_code metadata field to the schema.
-        schema.update({
-                'country_code': [toolkit.get_validator('ignore_missing'),
-                                toolkit.get_converter('convert_to_tags')('country_codes')]
-                })
-        return schema
     
-    def _add_custom_text(self, schema):
-        schema.update({
-            'custom_text': [toolkit.get_validator('ignore_missing'),
-                            toolkit.get_converter('convert_to_extras')]})
-        return schema
+    plugins.implements(plugins.IPackageController, inherit=True)
 
+    # IPackageController
+    def after_create(self, context, data):
+        
+        log.info('After Create GOOGLE DRIVE PLUGIN: Context: {}, Data: {}'.format(context, data))
+        url = data.get('url', None)
+        log.info('GOOGLE DRIVE PLUGIN url: {}'.format(url))
+        # detect google drive URLs
+        # https://docs.google.com/spreadsheets/d/1dUgDpVYDywwdqtWw0A8qqaCstgtb5GygsaTDRMIgUAo/edit?usp=drive_web&ouid=100530708543483213226
+        if url is None or not url.startswith('https://docs.google.com/spreadsheets'):
+            return
 
-    def create_package_schema(self):
-        # let's grab the default schema in our plugin
-        schema = super(Google_Drive_ImportPlugin, self).create_package_schema()
-        schema = self._add_country_codes(schema)
-        schema = self._add_custom_text(schema)
-        return schema
+        # si no tiene recursos, crearlos
+        resources = data.get('resources', None)
+        log.info('GOOGLE DRIVE PLUGIN Resources: {}'.format(resources))
+        if resources is None or len(resources) > 0:
+            return 
+        
     
-    def update_package_schema(self):
-        schema = super(Google_Drive_ImportPlugin, self).update_package_schema()
-        schema = self._add_country_codes(schema)
-        schema = self._add_custom_text(schema)
-        return schema
-
-    def show_package_schema(self):
-        schema = super(Google_Drive_ImportPlugin, self).show_package_schema()
-        schema = self._add_country_codes(schema)
-        schema = self._add_custom_text(schema)
-        return schema
     
-    def package_types(self):
-        # This plugin doesn't handle any special package types, it just
-        # registers itself as the default (above).
-        package_types = super(Google_Drive_ImportPlugin, self).package_types()
-        if package_types is None:
-            package_types = []
-        return package_types + ['Google Drive Sheet']
-
-    def is_fallback(self):
-        # Return True to register this plugin as the default handler for
-        # package types not handled by any other IDatasetForm plugin.
-        return True
-    
-    """
-    def package_form(self, package_type=None):
-        return 'package/form.html'
-    """
-
-    # IConfigurer
-
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'google_drive_import')
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.1.305',
+    version='0.1.312',
 
     description='''An extension to import datasets from Google Drive Sheets''',
     long_description=long_description,
